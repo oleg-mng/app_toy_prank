d4f4984e4511f6fcb4a33a5fe71b913868db91bf
oleg <legik7127@gmail.com>
2023-03-03 17:51:52 +0600

    created the class toy

---
+++ src/main/java/Toy.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Toy {
+
+        public static int id;
+        public static Random r;
+
+        protected String name;
+        protected int quantity;
+        protected int drop_frequency;
+
+        public Toy(String name, int hp) {
+            this.id = id;
+            this.name = name;
+            this.quantity = quantity;
+            this.drop_frequency = drop_frequency;
+        }
+}
