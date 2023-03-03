63c04a431f000866c5ab8c313de9a22414c5844b
oleg <legik7127@gmail.com>
2023-03-03 18:40:31 +0600

    added class instances

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,5 +1,13 @@
 public class Program {
     public static void main(String[] args) {
+        Toy Doll = new Toy(0,"Doll", 1,30);
+        Toy Robot = new Toy(1,"Robot", 2,20);
+        Toy Constructor = new Toy(2,"Constructor", 1,15);
+        Toy Soft_toy = new Toy(3,"Soft_toy", 1,35);
+        System.out.println(Doll.getInfo());
+        System.out.println(Robot.getInfo());
+        System.out.println(Constructor.getInfo());
+        System.out.println(Soft_toy.getInfo());
 
     }
 }
--- src/main/java/Toy.java
+++ src/main/java/Toy.java
@@ -1,18 +1,20 @@
-import java.util.Random;
 
 public class Toy {
 
-        public static int id;
-        public static Random r;
+        protected static int id;
 
         protected String name;
         protected int quantity;
         protected int drop_frequency;
 
-        public Toy(String name, int hp) {
+        public Toy(int id, String name, int quantity, int drop_frequency) {
             this.id = id;
             this.name = name;
             this.quantity = quantity;
             this.drop_frequency = drop_frequency;
         }
+        public String getInfo() {
+        return String.format("id: %d   name: %s   quantity: %d   drop_frequency: %d", id, name, quantity,
+                drop_frequency);
+    }
 }
