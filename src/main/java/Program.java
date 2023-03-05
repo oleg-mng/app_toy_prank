ed82647a7443f46927550549860cade763f420b8
oleg <legik7127@gmail.com>
2023-03-05 21:48:02 +0600

    added method addNewToy

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,13 +1,35 @@
+/**         Метод добавление новых игрушек и возможность изменения веса (частоты выпадения игрушки)
+        Возможность организовать розыгрыш игрушек.
+        Например, следующим образом:
+        С помощью метода выбора призовой игрушки – мы получаем эту призовую игрушку и записываем в список\массив.
+        Это список призовых игрушек, которые ожидают выдачи.
+        Еще у нас должен быть метод – получения призовой игрушки.
+        После его вызова – мы удаляем из списка\массива первую игрушку и сдвигаем массив. А эту игрушку записываем в текстовый файл.
+        Не забываем уменьшить количество игрушек
+*/
 public class Program {
     public static void main(String[] args) {
-        Toy Doll = new Toy(0,"Doll", 1,30);
-        Toy Robot = new Toy(1,"Robot", 2,20);
-        Toy Constructor = new Toy(2,"Constructor", 1,15);
-        Toy Soft_toy = new Toy(3,"Soft_toy", 1,35);
+        Toy Doll = new Toy(33,"Doll", 1,30);
+        Toy Robot = new Toy(7,"Robot", 2,20);
+        Toy Constructor = new Toy(23,"Constructor", 1,15);
+        Toy Soft_toy = new Toy(17,"Soft_toy", 1,35);
         System.out.println(Doll.getInfo());
         System.out.println(Robot.getInfo());
         System.out.println(Constructor.getInfo());
         System.out.println(Soft_toy.getInfo());
 
+        addNewToy(111,"Road", 1,50);
+        changeDropFrequency();
     }
+    public static void addNewToy(int number, String name, int qua, int drop) {
+        System.out.println("Get add toy...");
+        Toy toy = new Toy(number, name,qua, drop);
+        System.out.println(toy.getInfo());
+    }
+    public static void changeDropFrequency() {
+        System.out.println("Get change Drop Frequency of the toys");
+        int drop = Toy.r.nextInt(0, 5);
+        System.out.print("newDrop: " + drop);
+    }
+
 }
--- src/main/java/Toy.java
+++ src/main/java/Toy.java
@@ -1,11 +1,15 @@
+import java.util.Random;
 
 public class Toy {
 
-        protected static int id;
-
-        protected String name;
+    public static Random r;
+    protected int id;
+        public String name;
         protected int quantity;
         protected int drop_frequency;
+        static {
+            Random r = new Random();
+        }
 
         public Toy(int id, String name, int quantity, int drop_frequency) {
             this.id = id;
@@ -15,6 +19,6 @@ public class Toy {
         }
         public String getInfo() {
         return String.format("id: %d   name: %s   quantity: %d   drop_frequency: %d", id, name, quantity,
-                drop_frequency);
+                drop_frequency, getClass().getSimpleName());
     }
 }
